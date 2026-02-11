c947b556d9acb8d82dd14b06182d1ed200e352ab
Phill <felipe23freitas03@gmail.com>
2026-02-11 20:56:19 -0300

    Removendo linha vazia

--- start_platform.sh
+++ start_platform.sh
@@ -58,7 +58,6 @@ echo "-----------------------------------------"
 echo "Step 3 - Platform startup (docker-compose)"
 echo "-----------------------------------------"
 
-
 docker compose -f ${SCRIPT_DIR}/docker-compose.yml up -d --build
 
 if [ $? -ne 0 ]; then
